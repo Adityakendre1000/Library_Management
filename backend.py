2b57c9ddfb225fa87f796609f51158bf05964099
Adityakendre1000 <adityakendre1000@gmail.com>
2023-12-19 10:24:18 +0530

    initial commit

---
+++ backend.py
@@ -0,0 +1,39 @@
+import sqlite3
+
+class Database:
+    def __init__(self, db):
+        self.conn = sqlite3.connect(db)
+        self.cur = self.conn.cursor()
+        self.cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, isbn INTEGER, borrower TEXT, borrowed_date TEXT, return_date TEXT)")
+        self.conn.commit()
+
+    def view(self):
+        self.cur.execute("SELECT * FROM book")
+        rows = self.cur.fetchall()
+        return rows
+    
+    def insert(self, title, author, year, isbn):
+        self.cur.execute("INSERT INTO book VALUES (NULL,?,?,?,?)",(title, author, year, isbn))
+        self.conn.commit()
+        
+    def search(self, title="", author="", year="", isbn=""):
+        self.cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?", (title,author,year,isbn))
+        rows = self.cur.fetchall()
+        return rows
+    
+    def borrow_book(self, book_id, borrowed_date, borrower):
+        self.cur.execute("UPDATE book SET borrower=?, borrowed_date=? WHERE id=?", (borrower, borrowed_date, book_id))
+        self.conn.commit()
+    
+    def return_book(self, book_id, return_date):
+        self.cur.execute("UPDATE book SET borrower=NULL, borrowed_date=NULL, return_date=? WHERE id=?", (return_date, book_id))
+        self.conn.commit()
+    
+    def delete(self, id):
+        self.cur.execute("DELETE FROM book WHERE id=?",(id,))
+        self.conn.commit()
+    def update(self, id, title, author, year, isbn):
+        self.cur.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id=?",(title, author, year, isbn, id))
+        self.conn.commit()
+    def __del__(self):
+        self.conn.close()
